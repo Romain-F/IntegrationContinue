1feb91eb3f3b0a761aef7fcd5d2c7d3b8993d7a0
Romain <romain.feutray@hotmail.fr>
2020-05-12 16:27:44 +0200

    Initial commit

---
+++ IntegrationConsoleApp/IntegrationConsoleApp/Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationConsoleApp
+{
+	class Library
+	{
+		public int GetInt(string nombre)
+		{
+			return int.Parse(nombre) + 1;
+		}
+	}
+}
